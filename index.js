0350953197c7a7cfa0adc07c96e831f90ae115f9
vhaiek <haiek.vic@gmail.com>
2020-10-27 21:50:40 -0300

    excercise 6

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Excercise 1 - Variables y Operadores
+// Excercise 1 
 
 //a
-/* Crear dos variables numéricas y utilizar el operador suma para guardar el valor 
-de la suma de ambos números en una 3er variable.*/
+/*  Crear dos variables numéricas y utilizar el operador suma para guardar el valor 
+    de la suma de ambos números en una 3er variable.*/
 
 var number1= 11;
 var number2= 4;
@@ -10,15 +10,15 @@ var total= number1 + number2;
 console.log(number1 + " + " + number2 + " = " + total);
 
 //b
-/* Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+/*  Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
 var word1 ="environmental";
 var word2 = "law";
 var legalBranch = word1 + word2; 
 console.log(word1 + " " + word2);
 
 //c
-/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
- guardando el resultado la suma una 3er variable (utilizar length).*/
+/*  Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+    guardando el resultado la suma una 3er variable (utilizar length).*/
 var word3 ="civil";
 var word4 = "law";
 var legalBranch1 = word3 + word4;
